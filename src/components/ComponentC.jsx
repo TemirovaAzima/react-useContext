3c6da7ce5a5aa143170212d19772b9173512700d
TemirovaAzima <temirovaazima9@gmail.com>
2025-02-08 00:20:43 +0500

    Using createContext with useContext end

--- src/components/ComponentC.jsx
+++ src/components/ComponentC.jsx
@@ -1,36 +1,11 @@
-import {Data,Data2,Data3,Data4} from '../App.jsx'
-
+import {Data,Data2} from '../App.jsx'
+import {useContext} from 'react'
 export const ComponentC =()=>{
-
+    const userName= useContext(Data)
+    const userAge = useContext(Data2)
     return(
         <div>
-          <Data.Consumer>
-              {(name)=>{
-                  return (
-                      <Data2.Consumer>
-                          {(age)=>{
-                              return (
-                                  <Data3.Consumer>
-                                      {(dateOfBirth)=>{
-                                          return (
-                                              <Data4.Consumer>
-                                                  {(myFavouriteNumbers)=>{
-                                                      return (
-                                                          <h1>My name is : {name} and my age is {age} and year
-                                                              is {dateOfBirth.dateOfBirth} and my favourite numbers {myFavouriteNumbers[0]}</h1>
-                                                      )
-                                                  }}
-                                              </Data4.Consumer>
-
-                                          )
-                                      }}
-                                  </Data3.Consumer>
-                              )
-                          }}
-                      </Data2.Consumer>
-                  )
-              }}
-          </Data.Consumer>
+         <h1>My name is {userName} and my age is {userAge}</h1>
         </div>
     )
 }
